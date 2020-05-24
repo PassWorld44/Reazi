bbd9b2d2824aec30e30fd6eb56da8e17b2d53f19
Emile BONDU <emile.bondu@gmail.com>
2020-05-25 00:23:01 +0200

    ajouter le premake

---
+++ prmake5.lua
@@ -0,0 +1,106 @@
+workspace "Reazi"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Reazi"
+	location "Reazi"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemvertion "latest"
+
+		define
+		{
+			"RZ_PLATFORM_WINDOWS"
+			"RZ_BUILD_DLL"
+			"_WINDLL"
+		}
+
+		postbuilcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir.. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "RZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RZ_DISTG"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h"
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include"
+		"Reazi/src" 
+	}
+
+	links
+	{
+		"Reazi"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemvertion "latest"
+
+		define
+		{
+			"RZ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "RZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "RZ_DISTG"
+		optimize "On"
