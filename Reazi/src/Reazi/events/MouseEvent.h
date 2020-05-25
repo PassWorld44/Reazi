82ef9093cb8e9182953b37831c2f633c32a20ee1
Emile BONDU <emile.bondu@gmail.com>
2020-05-25 23:46:41 +0200

    ajout de mouse Event et c'est fini pr aujourd'hui

--- Reazi/src/Reazi/events/MouseEvent.h
+++ Reazi/src/Reazi/events/MouseEvent.h
@@ -1,1 +1,103 @@
 #pragma once
+
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Reazi
+{
+	class REAZI_API MouseMovedEvent : public Event
+	{
+	private:
+		float m_x, m_y;
+	public:
+
+		MouseMovedEvent(int x, int y) : m_x(x), m_y(y) {}
+		
+		inline float getX() { return m_x; }
+		inline float getY() { return m_y; }
+
+		std::string to_string() const override
+		{
+			std::stringstream ss;
+			ss << "MouseMovedEvent : x(" << m_x << "), y(" << m_y << ")";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseMoved)
+		EVENT_CLASS_CATEGORY( (int)EventCategory::EventCategoryMouse |(int) EventCategory::EventCategoryInput)
+	};
+
+	class REAZI_API MouseScolledEvent : public Event
+	{
+	private:
+		float m_xOffest, m_yOffset;
+	public:
+
+		MouseScolledEvent(int x, int y) : m_xOffest(x), m_yOffset(y) {}
+
+		inline float getXOffset() { return m_xOffest; }
+		inline float getY() { return m_yOffset; }
+
+		std::string to_string() const override
+		{
+			std::stringstream ss;
+			ss << "MouseScolledEvent : x offset(" << m_xOffest << "), y offset(" << m_yOffset << ")";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseScrolled)
+			EVENT_CLASS_CATEGORY((int)EventCategory::EventCategoryMouse | (int)EventCategory::EventCategoryInput)
+	};
+
+	class REAZI_API MouseButtonEvent : public Event
+	{
+	protected:
+		int m_button;
+	public:
+
+		MouseButtonEvent(int button) : m_button(button) {}
+
+		inline float getButton() { return m_button; }
+
+		std::string to_string() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonEvent : " << m_button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_CATEGORY((int)EventCategory::EventCategoryMouse | (int)EventCategory::EventCategoryInput)
+	};
+
+	class REAZI_API MousePressedButton : public MouseButtonEvent
+	{
+	public:
+		MousePressedButton(int button) : MouseButtonEvent(button) {}
+
+		std::string to_string() const override
+		{
+			std::stringstream ss;
+			ss << "MousePressedButton : " << m_button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonPressed)
+	};
+
+	class REAZI_API MouseReleasedButton : public MouseButtonEvent
+	{
+	public:
+		MouseReleasedButton(int button) : MouseButtonEvent(button) {}
+
+		std::string to_string() const override
+		{
+			std::stringstream ss;
+			ss << "MouseReleasedButton : " << m_button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonReleased)
+	};
+}
